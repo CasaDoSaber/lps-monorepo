92f8b30178d87869b6be36fb6a0da82df54052be
CasaDoSaber <jhalvesmeira@gmail.com>
2021-04-01 18:06:29 -0300

    .

--- christian-dunker/como-se-relacionar-melhor-christian-dunker/next.config.js
+++ christian-dunker/como-se-relacionar-melhor-christian-dunker/next.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    basePath: '/como-melhorar-relacoes',
+    basePath: '/como-melhorar-relacoess',
     env: {
         CASA_API_AUTHORIZATION:'$2a$05$t76bq95pBmGm3Y8PjthQiOs8mVgWvu5ZGIsIkLPNbiwH2e60u7UtO',  
         GTM_ID: "GTM-PXKGLX6",
-        NEXT_PUBLIC_VIMEO_CLASS1: 'https://player.vimeo.com/video/530951662'
+        NEXT_PUBLIC_VIMEO_CLASS1: 'https://player.vimeo.com/video/5309516622'
     },
   };
