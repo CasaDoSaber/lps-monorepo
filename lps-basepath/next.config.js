947a08dc665cd0f8141a82dcccd38d0069a73cd6
samsisle <samko9522@gmail.com>
2021-04-07 23:13:00 -0700

    Test multi-zones

--- lps-basepath/next.config.js
+++ lps-basepath/next.config.js
@@ -1,20 +1,23 @@
 module.exports = {
     async rewrites() {
         return [
-          
+          {
+            source: '/:path*',
+            destination: `/:path*`,
+          },
           {
             source: '/como-melhorar-relacoes',
-            destination: `https://lps-monorepo-como-melhorar-relacoes.vercel-support.app/`,
+            destination: `https://lps-monorepo-como-melhorar-relacoes.vercel-support.app/como-melhorar-relacoes`,
           },
           {
             source: '/como-melhorar-relacoes/:match*',
-            destination: `https://lps-monorepo-como-melhorar-relacoes.vercel-support.app/:match*`,
+            destination: `https://lps-monorepo-como-melhorar-relacoes.vercel-support.app//como-melhorar-relacoes/:match*`,
           }
 
         ]
       },    
       env: {
-        
+
         NEXT_PUBLIC_GA_TRACKING: 'UA-178602651-2',
         GTM_ID: "GTM-PXKGLX6",
     },
